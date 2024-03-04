120ccd6cd48f4242993027cd54494b338b487d19
Dmitry Lyfar <dmitry.lyfar@canonical.com>
2024-03-04 13:36:13 +1300

    Add tests for the workshop's conflicting changes check

--- internal/overlord/conflict/conflict_test.go
+++ internal/overlord/conflict/conflict_test.go
@@ -9,24 +9,140 @@ import (
 	"gopkg.in/check.v1"
 )
 
-type OperationSuite struct {
+type conflictSuite struct {
 	state   *state.State
 	project *workshopbackend.Project
 }
 
-var _ = check.Suite(&OperationSuite{})
+var _ = check.Suite(&conflictSuite{})
 
 func Test(t *testing.T) { check.TestingT(t) }
 
-func (s *OperationSuite) SetUpTest(c *check.C) {
+func (s *conflictSuite) newChange(kind string) *state.Change {
+	change := s.state.NewChange(kind, "test")
+	task := s.state.NewTask("test", "test")
+	task.Set("workshop", "ws")
+	change.AddTask(task)
+	change.Set("project-id", s.project.ProjectId)
+	return change
+}
+
+func (s *conflictSuite) SetUpTest(c *check.C) {
 	s.state = state.New(nil)
 	s.project = &workshopbackend.Project{Path: c.MkDir(), ProjectId: "42ws42ws"}
 }
 
-func (s *OperationSuite) TestResumeRefreshNothingInProgress(c *check.C) {
+func (s *conflictSuite) TestCheckChangeConflictNotFound(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	err := conflict.CheckChangeConflict(s.state, s.project.ProjectId, "ws", "")
+	c.Assert(err, check.IsNil)
+}
+
+func (s *conflictSuite) TestCheckChangeConflictFound(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	change := s.newChange("launch")
+
+	err := conflict.CheckChangeConflict(s.state, s.project.ProjectId, "ws", "")
+	c.Assert(err, check.NotNil)
+	conflictErr, ok := err.(*conflict.ChangeConflictError)
+	c.Assert(ok, check.Equals, true)
+	c.Assert(conflictErr.ChangeID, check.Equals, change.ID())
+	c.Assert(conflictErr.ChangeKind, check.Equals, "launch")
+	c.Assert(conflictErr.Workshop, check.Equals, "ws")
+	c.Assert(conflictErr.ProjectId, check.Equals, s.project.ProjectId)
+}
+
+func (s *conflictSuite) TestCheckChangeNoConflictWithReadyChange(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	change := s.newChange("launch")
+	change.SetStatus(state.DoneStatus)
+
+	err := conflict.CheckChangeConflict(s.state, s.project.ProjectId, "ws", "")
+	c.Assert(err, check.IsNil)
+}
+
+func (s *conflictSuite) TestCheckChangeConflictIgnoreChange(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	change := s.newChange("launch")
+
+	err := conflict.CheckChangeConflict(s.state, s.project.ProjectId, "ws", change.ID())
+	c.Assert(err, check.IsNil)
+}
+
+func (s *conflictSuite) TestResumeRefreshNothingInProgress(c *check.C) {
 	s.state.Lock()
 	defer s.state.Unlock()
 
 	_, err := conflict.ResumeRefresh(s.state, "ws", s.project.ProjectId, conflict.RefreshContinue)
 	c.Check(err, check.ErrorMatches, ".* no refresh in progress")
+}
+
+func (s *conflictSuite) TestResumeRefreshIncorrectMode(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	_, err := conflict.ResumeRefresh(s.state, "ws", s.project.ProjectId, conflict.RefreshTransactional)
+	c.Check(err, check.ErrorMatches, "cannot resume: only abort or continue can be used to resume the refresh operation")
+}
+
+func (s *conflictSuite) TestResumeRefreshWrongChangeKindInProgress(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	_ = s.newChange("launch")
+
+	_, err := conflict.ResumeRefresh(s.state, "ws", s.project.ProjectId, conflict.RefreshContinue)
+	c.Check(err, check.ErrorMatches, `.* no refresh in progress \("launch" is in progress\)`)
+}
+
+func (s *conflictSuite) TestResumeRefreshNoWaitingOnError(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	_ = s.newChange("refresh")
+
+	_, err := conflict.ResumeRefresh(s.state, "ws", s.project.ProjectId, conflict.RefreshContinue)
+	c.Check(err, check.ErrorMatches, ".* no refresh is waiting on error")
+}
+
+func (s *conflictSuite) TestResumeRefreshContinue(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	change := s.newChange("refresh")
+	task := change.Tasks()[0]
+	task.SetToWait(state.HoldStatus)
+
+	_, err := conflict.ResumeRefresh(s.state, "ws", s.project.ProjectId, conflict.RefreshContinue)
+	c.Assert(err, check.IsNil)
+	c.Assert(task.Status(), check.Equals, state.HoldStatus)
+}
+
+func (s *conflictSuite) TestResumeRefreshAbort(c *check.C) {
+	s.state.Lock()
+	defer s.state.Unlock()
+
+	change := s.newChange("refresh")
+	task := change.Tasks()[0]
+	task.SetToWait(state.DoingStatus)
+	task2 := s.state.NewTask("do", "to do")
+	// emulate tasks to be done down the lane
+	// if abort is called for the change, the task will
+	// be reverted to hold.
+	task2.SetStatus(state.DoStatus)
+	change.AddTask(task2)
+	change.SetStatus(state.WaitStatus)
+
+	_, err := conflict.ResumeRefresh(s.state, "ws", s.project.ProjectId, conflict.RefreshAbort)
+	c.Assert(err, check.IsNil)
+	c.Assert(task.Status(), check.Equals, state.ErrorStatus)
+	c.Assert(task2.Status(), check.Equals, state.HoldStatus)
 }
