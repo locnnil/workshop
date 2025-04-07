7a1c1f3a1e29d57b9284f58886f46b6918c5ff9a
Jonathan Conder <jonathan.conder@canonical.com>
2025-04-08 10:44:48 +1200

    Add revert tests
    Previously this "test suite" rejected the -check.v flag.

--- internal/revert/revert_test.go
+++ internal/revert/revert_test.go
@@ -1,30 +1,47 @@
 package revert_test
 
 import (
-	"fmt"
+	"testing"
+
+	"gopkg.in/check.v1"
 
 	"github.com/canonical/workshop/internal/revert"
 )
 
-func ExampleReverter_fail() {
-	revert := revert.New()
-	defer revert.Fail()
-
-	revert.Add(func() { fmt.Println("1st step") })
-	revert.Add(func() { fmt.Println("2nd step") })
-
-	// Revert functions are run in reverse order on return.
-	// Output: 2nd step
-	// 1st step
+type revertSuite struct {
+	result []string
 }
 
-func ExampleReverter_success() {
+var _ = check.Suite(&revertSuite{})
+
+func TestRevert(t *testing.T) { check.TestingT(t) }
+
+func (r *revertSuite) SetUpTest(c *check.C) {
+	r.result = []string{}
+}
+
+func (r *revertSuite) TestRevertFail(c *check.C) {
+	defer func() {
+		c.Assert(r.result, check.DeepEquals, []string{"2nd step", "1st step"})
+	}()
+
 	revert := revert.New()
 	defer revert.Fail()
 
-	revert.Add(func() { fmt.Println("1st step") })
-	revert.Add(func() { fmt.Println("2nd step") })
+	// Revert functions are run in reverse order on return.
+	revert.Add(func() { r.result = append(r.result, "1st step") })
+	revert.Add(func() { r.result = append(r.result, "2nd step") })
+}
 
-	revert.Success() // Revert functions added are not run on return.
-	// Output:
+func (r *revertSuite) TestRevertSuccess(c *check.C) {
+	defer func() {
+		c.Assert(r.result, check.DeepEquals, []string{})
+	}()
+
+	revert := revert.New()
+	defer revert.Fail()
+
+	revert.Add(func() { r.result = append(r.result, "ignored") })
+
+	revert.Success()
 }
