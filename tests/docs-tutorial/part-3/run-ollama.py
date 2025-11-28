937ce8731ffb0f6db826889e851958f1e9b292a3
Artem Konev <artem.konev@canonical.com>
2025-11-28 16:14:34 +0000

    Doc: Improve test for tutorial part 3

---
+++ tests/docs-tutorial/part-3/run-ollama.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+# Check if Ollama is running
+response = requests.get('http://localhost:11434/api/version')
+print(f"Ollama version: {response.json()['version']}")
+
+# Generate text with the tinyllama model, installed in Part 1
+data = {
+    "model": "tinyllama",
+    "prompt": "Why is the sky blue?",
+    "stream": False
+}
+response = requests.post('http://localhost:11434/api/generate', json=data)
+print(response.json()['response'])
